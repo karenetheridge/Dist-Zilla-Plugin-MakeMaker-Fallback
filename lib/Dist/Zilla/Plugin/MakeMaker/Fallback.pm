423ff3cbba344b28c32bee0c57d773b544f21f7b
Karen Etheridge <ether@cpan.org>
2013-09-22 00:10:35 -0700

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.024, github profile.

---
+++ lib/Dist/Zilla/Plugin/MakeMaker/Fallback.pm
@@ -0,0 +1,54 @@
+use strict;
+use warnings;
+package Dist::Zilla::Plugin::MakeMaker::Fallback;
+# ABSTRACT: ...
+# vim: set ts=8 sw=4 tw=78 et :
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::MakeMaker::Fallback;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=over 4
+
+=item * C<foo>
+
+...
+
+=back
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-MakeMaker-Fallback>
+(or L<bug-Dist-Zilla-Plugin-MakeMaker-Fallback@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-MakeMaker-Fallback@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=begin :list
+
+* L<foo>
+
+=end :list
+
+=cut
