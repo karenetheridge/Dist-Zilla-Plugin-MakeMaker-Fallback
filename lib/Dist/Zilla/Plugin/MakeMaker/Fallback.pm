980dc60174d76b03d9b6a3af778aa92b9494b38b
Karen Etheridge <ether@cpan.org>
2015-12-25 18:22:49 -0800

    include our version explicitly in dumped configs if we were subclassed

--- lib/Dist/Zilla/Plugin/MakeMaker/Fallback.pm
+++ lib/Dist/Zilla/Plugin/MakeMaker/Fallback.pm
@@ -15,20 +15,18 @@ use List::Util 'first';
 use version;
 use namespace::autoclean;
 
-# no point in bothering with this until we have config variables
-#around dump_config => sub
-#{
-#    my $orig = shift;
-#    my $self = shift;
-#
-#    my $config = $self->$orig;
-#
-#    $config->{+__PACKAGE__} = {
-#        # ...
-#    };
-#
-#    return $config;
-#};
+around dump_config => sub
+{
+    my ($orig, $self) = @_;
+    my $config = $self->$orig;
+
+    my $data = {
+        blessed($self) ne __PACKAGE__ ? ( version => $VERSION ) : (),
+    };
+    $config->{+__PACKAGE__} = $data if keys %$data;
+
+    return $config;
+};
 
 sub register_prereqs
 {
